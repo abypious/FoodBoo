080ecf0c2f25624c69bd825e99b0388ecd9df6ea
Aby Pious <abypious3@gmail.com>
2025-11-20 18:20:23 +0530

    all updates are done

--- backend/src/main/java/com/fooboo/model/Booking.java
+++ backend/src/main/java/com/fooboo/model/Booking.java
@@ -1,6 +1,5 @@
 package com.fooboo.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
--- backend/src/main/java/com/fooboo/model/Review.java
+++ backend/src/main/java/com/fooboo/model/Review.java
@@ -1,6 +1,5 @@
 package com.fooboo.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
